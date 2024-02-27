f3263db0a3b3f08e5e9fbe8b4eb1159b594a4414
Krishanu2206 <krishanub03@gmail.com>
2024-02-27 12:12:26 +0530

    message

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ const sessionoptions={
     saveUninitialized:true,
     cookie:{
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000, //in milliseconds
         httpOnly:true,
     }
 };
