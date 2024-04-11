2d47c4ef73889156c95793a635488de58cf1764e
Krishanu2206 <krishanub03@gmail.com>
2024-04-11 20:42:03 +0530

    changed

--- cloudconfig.js
+++ cloudconfig.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name:process.env.cloud_name,
+    cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.CLOUD_API_KEY,
     api_secret:process.env.CLOUD_API_SECRET
 })
